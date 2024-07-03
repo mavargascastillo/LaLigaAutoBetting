82152021a6522c927622c930b710e2896cc8d5da
Miguel Angel Vargas <miguelxtype@gmail.com>
2024-07-03 21:26:35 +0000

    checked all functions with the new betfunctions module

--- terminal.py
+++ terminal.py
@@ -1,10 +1,14 @@
 import classes.data_handler as dh
-import funciones2 as fx
+import betfunctions as fx
+
 
 path = r"/workspace/LaLigaAutoBetting/Goles.xlsx"
 data = dh.DataHandler(path)
 
 matchpairs = data.extract_enfrentamientos()
 apuestas_jornada = fx.bets_jornada(path, matchpairs)
+apuestas_finales = fx.find_better_bet(apuestas_jornada)
+combinadas_sin_cuota = fx.random_pairing(apuestas_finales)
+combinadas_con_cuota = fx.calculate_combinadas(combinadas_sin_cuota)
 
-print(apuestas_jornada)
+print(combinadas_con_cuota)
